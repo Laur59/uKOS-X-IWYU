66a5bf55a238b8c2b9bbc44beff9235d639f02c4
Laur59 <148864407+Laur59@users.noreply.github.com>
2026-06-02 10:44:16 +0300

    Merge remote-tracking branch 'upstream/master' into main - 0.5.6

--- Third_Parties/LVGL/Construction/RK050HR18_5_Inches/CORTEX_M85/lv_conf.h
+++ Third_Parties/LVGL/Construction/RK050HR18_5_Inches/CORTEX_M85/lv_conf.h
@@ -1,14 +1,56 @@
 /*
- * SPDX-License-Identifier: MIT
- * SPDX-FileCopyrightText: 2025-2026 Edo. Franzi
- *
- * Goal:     Basic LVGL configuration
- *           Set 16 & 26 pixel fonts.
- *           Internal memory allocator
- *
- *           Configuration file for LVGL v9.5.x
- *           (based on lv_conf_template.h v9.5.0)
- */
+; lv_conf.
+; ========
+
+; SPDX-License-Identifier: MIT
+; SPDX-FileCopyrightText: 2025-2026 Edo. Franzi
+
+;------------------------------------------------------------------------
+; Author:   Edo. Franzi     The 2026-01-01
+; Modifs:
+;
+; Project:  uKOS-X
+; Goal:     Basic LVGL configuration
+;           Set 16 & 26 pixel fonts.
+;           Internal memory allocator
+;
+;           Configuration file for LVGL v9.5.x
+;           (based on lv_conf_template.h v9.5.0)
+;
+;   (c) 2025-2026, Edo. Franzi
+;   --------------------------
+;                                              __ ______  _____
+;   Edo. Franzi                         __  __/ //_/ __ \/ ___/
+;   5-Route de Cheseaux                / / / / ,< / / / /\__ \
+;   CH 1400 Cheseaux-Noréaz           / /_/ / /| / /_/ /___/ /
+;                                     \__,_/_/ |_\____//____/
+;   edo.franzi@ukos.ch
+;
+;   Description: Lightweight, real-time multitasking operating
+;   system for embedded microcontroller and DSP-based systems.
+;
+;   Permission is hereby granted, free of charge, to any person
+;   obtaining a copy of this software and associated documentation
+;   files (the "Software"), to deal in the Software without restriction,
+;   including without limitation the rights to use, copy, modify,
+;   merge, publish, distribute, sublicense, and/or sell copies of the
+;   Software, and to permit persons to whom the Software is furnished
+;   to do so, subject to the following conditions:
+;
+;   The above copyright notice and this permission notice shall be
+;   included in all copies or substantial portions of the Software.
+;
+;   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+;   BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+;   ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+;   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;   SOFTWARE.
+;
+;------------------------------------------------------------------------
+*/
 
 #if 1
 
@@ -20,7 +62,7 @@
 
 // Color depth: 1 (I1), 8 (L8), 16 (RGB565), 24 (RGB888), 32 (XRGB8888)
 //
-#define LV_COLOR_DEPTH                                  16
+#define LV_COLOR_DEPTH                                  32
 
 // STDLIB WRAPPER SETTINGS
 // =======================
@@ -74,8 +116,8 @@
 
 // Resolution
 //
-#define LV_HOR_RES_MAX                                  320
-#define LV_VER_RES_MAX                                  240
+#define LV_HOR_RES_MAX                                  800
+#define LV_VER_RES_MAX                                  480
 
 // OPERATING SYSTEM
 // ================
@@ -91,9 +133,9 @@
 #define LV_DRAW_BUF_ALIGN                               4
 
 #define LV_DRAW_TRANSFORM_USE_MATRIX                    0
-#define LV_DRAW_LAYER_SIMPLE_BUF_SIZE                   (24 * 1024)
+#define LV_DRAW_LAYER_SIMPLE_BUF_SIZE                   (64 * 1024)
 #define LV_DRAW_LAYER_MAX_MEMORY                        0
-#define LV_DRAW_THREAD_STACK_SIZE                       (8 * 1024)
+#define LV_DRAW_THREAD_STACK_SIZE                       (16 * 1024)
 #define LV_DRAW_THREAD_PRIO                             LV_THREAD_PRIO_HIGH
 
 #define LV_USE_DRAW_SW                                  1
